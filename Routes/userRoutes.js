04e82ab9b78fb9d0a4d5aba86ee75e9a765a521d
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-09-15 18:21:42 +0530

    cors setup

---
+++ Routes/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const userController = require("../Controllers/userController");
+
+const router = express.Router();
+
+router.post("/signup", userController.signup);
+module.exports = router;
