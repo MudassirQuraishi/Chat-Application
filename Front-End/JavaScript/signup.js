99816d12b1077b1a17203c1fef4de75714979dbd
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-09-15 19:36:49 +0530

    login front end

--- Front-End/JavaScript/signup.js
+++ Front-End/JavaScript/signup.js
@@ -3,12 +3,14 @@ const username = document.getElementById("username-signup");
 const email = document.getElementById("email-signup");
 const phoneNumber = document.getElementById("contact-number");
 const password = document.getElementById("password");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const signupButton = document.getElementById("signup-button");
 
 // signin_information
-
-const signupButton = document.getElementById("signup-button");
+const siginName = document.getElementById("signin-email");
+const siginPassword = document.getElementById("signin-password");
 const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
+const signinButton = document.getElementById("signin-button");
 const container = document.querySelector(".container");
 
 //Event Listeners
@@ -22,6 +24,7 @@ const container = document.querySelector(".container");
   });
 
   signupButton.addEventListener("click", signup);
+  signinButton.addEventListener("click", signin);
 }
 //signup function
 async function signup(e) {
@@ -43,17 +46,54 @@ async function signup(e) {
       };
       const response = await axios.post(
         "http://localhost:3000/user/signup",
-        { credentials: "include" },
         signupDetails
       );
       console.log(response.status);
       if (response.status === 200 || response.status === 201) {
+        if (response.status === 200) {
+          alert("Created successfully, Login to Continue");
+        } else if (response.status === 201) {
+          alert("User already exits, Login to Continue");
+        }
         container.classList.remove("sign-up-mode");
       }
     } catch (error) {
       console.log(error);
       console.log("Error Signing Up");
       alert("Error Signing Up");
+    }
+  }
+}
+async function signin(e) {
+  e.preventDefault();
+  if (siginName.value === "" || siginPassword.value === "") {
+    alert("Please Fill All Details");
+  } else {
+    try {
+      const siginDetials = {
+        username: siginName.value,
+        password: siginPassword.value,
+      };
+      const response = await axios.post(
+        "http://localhost:3000/user/login",
+        siginDetials
+      );
+      if (response.status === 200) {
+        localStorage.setItem("token", response.data.encryptedId);
+        alert("Logged In Successfully");
+      } else if (response.status === 401) {
+        siginPassword.value = "";
+        alert("Invalid password");
+      } else if (response.status === 404) {
+        siginName.value = "";
+        siginPassword = "";
+        alert("User not found, Please sign up");
+        container.classList.add("sign-up-mode");
+      }
+    } catch (error) {
+      console.log("Error During Login");
+      console.log(error);
+      alert("Login Failed please refresh");
     }
   }
 }
